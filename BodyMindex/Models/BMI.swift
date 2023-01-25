000d88bc35809924a83d5d16ee71fa1c796d7edd
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-01-24 23:24:21 -0600

    Creación de la estrutura BMI
    Se agregó un archivo para el MVC y se aplicó el uso de métodos de trabajo con Optionals en el BodyMindexBrain

---
+++ BodyMindex/Models/BMI.swift
@@ -0,0 +1,10 @@
+/* BMI.swift --> BodyMindex. Created by Miguel Torres on 24/01/23. */
+
+import UIKit
+
+struct BMI {
+    // Definimos los atributos como constantes porque una vez que se crea el objeto de esta estructura, no cambia ningún valor para dicho objeto, solo se usa una vez.
+    var value: Float
+    let advice: String
+    let color: UIColor
+}
--- BodyMindex/Models/BodyMindexBrain.swift
+++ BodyMindex/Models/BodyMindexBrain.swift
@@ -3,17 +3,35 @@
 import Foundation
 
 struct BodyMindexBrain {
-    // Variable que va a guardar el valor del IMC calculado en la siguiente función:
-    var bmiValue = "0.0"
-    
-    // Método para calcular el BMI
-    mutating func calculateBMI(height: Float, weight: Float) {
-        let bmi = weight / pow(height, 2) // Alternativa: BMI = weight / (height * height)
-        // Pasamos el valor del resultado a la variable externa que puede guardar el valor después de finalizarse el método.
-        bmiValue = String(format: "%.1f", bmi)
-    }
+    // Creamos un objeto opcional de tipo BMI.
+    var bmi: BMI?
     
     func getBMIValue() -> String {
+        // Usamos el método de "Nil coalescing operator" para abrir la variable opcional, dándole un valor por default en caso de que sea nula.
+        let bmiValue = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiValue
     }
+    
+    mutating func calculateBMI(_ height: Float, _ weight: Float) {
+        bmi?.value = weight / pow(height, 2)
+    }
 }
+
+
+/* Método alternativo para el cálculo del IMC y su retorno:
+ 
+ // Variable que va a guardar el valor del IMC calculado en la siguiente función:
+ var bmiValue = "0.0"
+ 
+ // Método para calcular el BMI
+ mutating func calculateBMI(height: Float, weight: Float) {
+     let bmi = weight / pow(height, 2) // Alternativa: BMI = weight / (height * height)
+     // Pasamos el valor del resultado a la variable externa que puede guardar el valor después de finalizarse el método.
+     bmiValue = String(format: "%.1f", bmi)
+ }
+ 
+ func getBMIValue() -> String {
+     return bmiValue
+ }
+ 
+ */
